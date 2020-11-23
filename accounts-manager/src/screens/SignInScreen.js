3509c884ba5992b7b6b5c67e39a1d0e109e7ad46
Zadid Bin Azad <43434510+Zenhance@users.noreply.github.com>
2020-11-23 12:27:51 +0600

    Initial project layout

---
+++ accounts-manager/src/screens/SignInScreen.js
@@ -0,0 +1,13 @@
+import React from "react";
+import {Text,Button} from "react-native-elements";
+import {View,StyleSheet} from "react-native";
+
+const SignInScreen = () => {
+    return (
+        <View>
+            <Text>Sign In Screen</Text>
+        </View>
+    );
+};
+
+export default SignInScreen;
---
+++ accounts-manager/src/screens/SplashScreen.js
@@ -0,0 +1,13 @@
+import React from "react";
+import {Text,Button} from "react-native-elements";
+import {View,StyleSheet} from "react-native";
+
+const SplashScreen = () => {
+    return(
+        <View>
+            <Text>Splash Screen</Text>
+        </View>
+    );
+};
+
+export default SplashScreen;
